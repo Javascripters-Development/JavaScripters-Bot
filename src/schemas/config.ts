5039e97ad5e7d4117b01b851e27003f180c1cd44
Gus Libens <27970303+goestav@users.noreply.github.com>
2023-08-23 17:56:39 +0200

    refactor: merge `gatewayEnabled` with `gatewayChannel` column
    If the `gatewayChannel` is NULL then we can assume the gateway is disabled, otherwise enabled.

--- src/schemas/config.ts
+++ src/schemas/config.ts
@@ -1,8 +1,7 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const Config = sqliteTable("guildConfig", {
 	id: text("guildId").primaryKey().notNull(),
 	gatewayChannel: text("gatewayChannel"),
-	gatewayEnabled: integer("gatewayEnabled", { mode: "boolean" }).default(false),
 	gatewayContent: text("gatewayContent"),
 });
