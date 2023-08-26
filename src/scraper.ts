44980d79e4dcb2a016572dffb4c719ced8b032ff
Morgân von Brylân <julesr1230@gmail.com>
2023-08-26 12:09:02 +0200

    scraper: accept cache curation of 0

--- src/scraper.ts
+++ src/scraper.ts
@@ -12,9 +12,9 @@ if (process.env.SCRAPE_CACHE) {
 		throw new TypeError(
 			"Environment variable SCRAPE_CACHE should be an integer",
 		);
-	if (+process.env.SCRAPE_CACHE < 1)
+	if (+process.env.SCRAPE_CACHE < 0)
 		throw new RangeError(
-			"Environment variable SCRAPE_CACHE should be a positive integer",
+			"Environment variable SCRAPE_CACHE should be 0 or positive",
 		);
 }
 
@@ -22,15 +22,17 @@ const CACHE_DURATION = +(process.env.SCRAPE_CACHE || 1) * 3600_000;
 const cache = new Map<string, CheerioAPI>();
 
 export default async function scrape(url: string, skipCache = false) {
-	if (!skipCache) {
+	if (CACHE_DURATION && !skipCache) {
 		const cached = cache.get(url);
 		if (cached) return cached;
 	}
 
 	const html = await fetch(url).then((r) => r.text());
 	const crawler = load(html);
-	cache.set(url, crawler);
-	setTimeout(cache.delete.bind(cache, url), CACHE_DURATION);
+	if (CACHE_DURATION) {
+		cache.set(url, crawler);
+		setTimeout(cache.delete.bind(cache, url), CACHE_DURATION);
+	}
 	return crawler;
 }
 
