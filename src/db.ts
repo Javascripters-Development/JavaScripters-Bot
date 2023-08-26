1e76e8a1a1f8cab204df60173e3d26e016e28f9f
Gus Libens <27970303+goestav@users.noreply.github.com>
2023-08-26 22:45:28 +0200

    refactor: add `ORM_DEBUG` environmental variable

--- src/db.ts
+++ src/db.ts
@@ -4,5 +4,7 @@ import { Database } from "bun:sqlite";
 const sqlite = new Database("main.db");
 sqlite.run("PRAGMA journal_mode = WAL");
 sqlite.run("PRAGMA optimize");
-const db: BunSQLiteDatabase = drizzle(sqlite);
+const db: BunSQLiteDatabase = drizzle(sqlite, {
+	logger: process.env.ORM_DEBUG === "true",
+});
 export default db;
