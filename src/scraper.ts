b315acd70135ed3eedeca186dc303bf2a604817b
Morgân von Brylân <julesr1230@gmail.com>
2023-08-27 14:16:40 +0200

    scraper: improves the cache

--- src/scraper.ts
+++ src/scraper.ts
@@ -19,20 +19,19 @@ if (process.env.SCRAPE_CACHE) {
 }
 
 const CACHE_DURATION = +(process.env.SCRAPE_CACHE || 1) * 3600_000;
-const cache = new Map<string, CheerioAPI>();
+const cache = new Map<string, { fetchDate: number; crawler: CheerioAPI }>();
 
 export default async function scrape(url: string, skipCache = false) {
+	const now = Date.now();
 	if (CACHE_DURATION && !skipCache) {
 		const cached = cache.get(url);
-		if (cached) return cached;
+		if (cached && cached.fetchDate + CACHE_DURATION < now) {
+			return cached.crawler;
+		}
 	}
-
 	const html = await fetch(url).then((r) => r.text());
 	const crawler = load(html);
-	if (CACHE_DURATION) {
-		cache.set(url, crawler);
-		setTimeout(cache.delete.bind(cache, url), CACHE_DURATION);
-	}
+	if (CACHE_DURATION) cache.set(url, { fetchDate: now, crawler });
 	return crawler;
 }
 
