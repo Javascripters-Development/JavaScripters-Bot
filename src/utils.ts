7e87006fcc1d74b90a388eccba6b7f4c56c110dc
AsyncBanana <jej.jackson.08@gmail.com>
2023-12-27 18:15:07 -0800

    chore: update dependencies and replace rome with biome

--- src/utils.ts
+++ src/utils.ts
@@ -8,9 +8,9 @@ import { sql, eq } from "drizzle-orm";
  * castArray(1) // [1]
  * castArray(['Hello', 'world']) // ['Hello', 'world']
  */
-// rome-ignore lint/suspicious/noExplicitAny: can be an array of any type
+// biome-ignore lint/suspicious/noExplicitAny: can be an array of any type
 export const castArray = <T>(value: T): T extends any[] ? T : T[] => {
-	// rome-ignore lint/suspicious/noExplicitAny: can be an array of any type
+	// biome-ignore lint/suspicious/noExplicitAny: can be an array of any type
 	return (Array.isArray(value) ? value : [value]) as T extends any[] ? T : T[];
 };
 
