b4a0c52691a358a7d9e3fbf7813e119805714c02
Goestav <27970303+goestav@users.noreply.github.com>
2024-01-04 04:05:45 +0100

    feat: suggestion system (#16)
    * refactor: add suggestion class
    
    * refactor: add suggest command
    
    * refactor: add `ORM_DEBUG` environmental variable
    
    * refactor: add suggestion button listener
    
    * refactor: add suggestion manager role
    
    * refactor: get rid of magic numbers
    
    * refactor: improve type safety
    
    * refactor: remove unnecessary arrays
    
    * refactor: temporary fix for `.get()`
    
    The `.get()` method does not work in the ORM so it will internally select the first row returned
    with `.all()`.
    
    * refactor: add suggestion database tables
    
    * refactor: export client
    
    * refactor: add UserError class
    
    * refactor: add `getKeyByValue` utility function
    
    * refactor: add abstraction for suggestion system
    
    * refactor: add command and listener
    
    * refactor: store votes in columns
    
    * refactor: cleanup old code
    
    * refactor: rename type to `UpdatedSuggestionStatus`
    
    * refactor: rename Suggestion to DiscordSuggestion
    
    * refactor: add abstract suggestion class
    
    * refactor: improve code documentation
    
    * refactor: improve discordjs checks to avoid errors
    
    * refactor: move utility to suggestion manager
    
    * build: fix typo
    
    * refactor: remove unused imports
    
    * refactor: improve naming + add code documentation
    
    * refactor: remove in-memory storage
    
    * refactor: export as utility functions
    
    * refactor: follow project file naming convention
    
    * refactor: fix TS errors
    
    * refactor: fix drizzle bug
    
    * refactor: fix bug with updating the suggestion
    
    * refactor: move update message method
    
    * refactor: cleanup + bugfixes
    
    * refactor: move default emoji to database schema
    
    * refactor: add drizzle custom type
    
    * refactor: remove error handling
    
    * refactor: improve code docs
    
    * refactor: remove error handling
    
    * refactor: add foreign key for guild id
    
    * refactor: remove votable class
    
    * refactor: use correct dataType for string set custom type
    
    * refactor: keep database suggestion in-memory to reduce queries
    
    * refactor: move prepared sql statement
    
    * refactor: cleanup
    
    * refactor: rename `raw` to `data`

---
+++ src/schemas/suggestion.ts
@@ -0,0 +1,51 @@
+import { sqliteTable, text, int } from "drizzle-orm/sqlite-core";
+import { sql, type InferInsertModel, type InferSelectModel } from "drizzle-orm";
+import { stringSet } from "../utils/drizzle.ts";
+import { Config } from "./config.ts";
+
+export const SUGGESTION_STATUS = {
+	POSTED: "POSTED",
+	ACCEPTED: "ACCEPTED",
+	REJECTED: "REJECTED",
+} as const satisfies Record<string, string>;
+
+export type SuggestionStatus =
+	typeof SUGGESTION_STATUS[keyof typeof SUGGESTION_STATUS];
+export type UpdatedSuggestionStatus = Exclude<SuggestionStatus, "POSTED">;
+
+const SUGGESTION_STATUS_VALUES = Object.values(SUGGESTION_STATUS) as [
+	"POSTED",
+	"ACCEPTED",
+	"REJECTED",
+];
+
+export const Suggestion = sqliteTable("suggestion", {
+	id: int("id").primaryKey({ autoIncrement: true }).notNull(),
+
+	title: text("title").notNull(),
+	description: text("description"),
+
+	guildId: text("guildId").references(() => Config.id, { onDelete: 'cascade' }).notNull(),
+	channelId: text("channelId").notNull(),
+	messageId: text("messageId").notNull(),
+	userId: text("userId").notNull(),
+
+	status: text("status", { enum: SUGGESTION_STATUS_VALUES })
+		.default("POSTED")
+		.notNull(),
+	statusReason: text("statusReason"),
+	statusUserId: text("statusUserId"),
+
+	upvotedBy: stringSet("upvotedBy"),
+	downvotedBy: stringSet("downvotedBy"),
+
+	updatedAt: int("updatedAt", { mode: "timestamp" })
+		.default(sql`(strftime('%s', 'now'))`)
+		.notNull(),
+	createdAt: int("createdAt", { mode: "timestamp" })
+		.default(sql`(strftime('%s', 'now'))`)
+		.notNull(),
+});
+
+export type SuggestionSelect = InferSelectModel<typeof Suggestion>;
+export type SuggestionInsert = InferInsertModel<typeof Suggestion>;
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { hyperlink as djsHyperlink, hideLinkEmbed } from "discord.js";
 import db from "./db.ts";
 import { Config } from "./schemas/config.ts";
 import { sql, eq } from "drizzle-orm";
@@ -19,3 +20,55 @@ export const getConfig = db
 	.from(Config)
 	.where(eq(Config.id, sql.placeholder("guildId")))
 	.prepare();
+
+// TEMP: use .all() and select the first row manually, .get() does not work
+getConfig.get = (placeholderValues?: Record<string, unknown> | undefined) => {
+	return getConfig.all(placeholderValues)[0];
+};
+
+/**
+ * Formats the content and the URL into a masked URL without embed.
+ * @see {@link djsHyperlink}.
+ */
+export const hyperlink = <C extends string, U extends string>(
+	content: C,
+	url: U,
+) => djsHyperlink(content, hideLinkEmbed(url));
+
+/**
+ * Capitalizes the first letter of a string.
+ *
+ * @example
+ * capitalizeFirstLetter('hello world') // "Hello world"
+ */
+export const capitalizeFirstLetter = <T extends string>(
+	value: T,
+): Capitalize<T> => (value[0].toUpperCase() + value.slice(1)) as Capitalize<T>;
+
+/**
+ * Utility for human readable time values.
+ *
+ * @example
+ * setTimeout(() => {}, Time.Hour * 2) // 2 hour timer
+ * setTimeout(() => {}, Time.Day) // 1 day timer
+ */
+export enum Time {
+	Second = 1000,
+	Minute = Time.Second * 60,
+	Hour = Time.Minute * 60,
+	Day = Time.Hour * 24,
+	Week = Time.Day * 7,
+	Year = Time.Day * 365,
+	Month = Time.Year / 12,
+}
+
+/**
+ * Find object key from value.
+ *
+ * @example
+ * getKeyByValue({ a: 1, b: 2, c: 3 }, 2) // "b"
+ */
+export const getKeyByValue = <const T>(
+	object: Record<PropertyKey, T>,
+	value: T,
+) => Object.keys(object).find((key) => object[key] === value);
