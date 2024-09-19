20e81c70add5749e5a2db57efcf4bacb2673a3c8
Morgân von Brylân <julesr1230@gmail.com>
2024-09-19 20:24:49 +0200

    feat/utils: improves truncate()

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,13 +1,15 @@
 /**
  * Truncate text to a certain amount of characters.
+ * @param text The text to truncate if it's too long.
+ * @param maxCharacters The output's maximum length (inclusive)
+ * @param ellipsis The character(s) to use as ellipsis.
  *
  * @example
- * truncate('Hello world!', 8) // 'Hello...'
+ * truncate("Hello world!", 8) // 'Hello w…'
+ * truncate("Hello world!", 8, "...") // 'Hello...'
  */
-export const truncate = (text: string, maxCharacters: number) => {
-	if (text.length < maxCharacters) return text;
-
-	const ellipsis = "...";
-
-	return text.slice(0, maxCharacters - ellipsis.length) + ellipsis;
-};
+export function truncate(text: string, maxCharacters: number, ellipsis = "…") {
+	return text.length <= maxCharacters
+		? text
+		: text.slice(0, maxCharacters - ellipsis.length) + ellipsis;
+}
